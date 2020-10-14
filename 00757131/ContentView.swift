febabf8ff2755701b9d0b324a8d391ec962e01cc
aaaaaaaaaaa <aaaaa>
2020-10-14 10:16:28 +0800

    Initial Commit

---
+++ 00757131/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00757131
+//
+//  Created by User09 on 2020/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00757131/_0757131App.swift
@@ -0,0 +1,17 @@
+//
+//  _0757131App.swift
+//  00757131
+//
+//  Created by User09 on 2020/10/14.
+//
+
+import SwiftUI
+
+@main
+struct _0757131App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
